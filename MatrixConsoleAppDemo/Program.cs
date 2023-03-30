bb9f0aa4e256bd298fe26926b3e0893a9bde9e59
Dominik Zemlik <dominik.zemlik.dev@gmail.com>
2023-03-30 18:10:37 +0200

    Add project files.

---
+++ MatrixConsoleAppDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MatrixConsoleAppDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
